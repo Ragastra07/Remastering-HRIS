bf8ceb3115c0bf8dc0aad4d25243c7d5dae0a455
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-03 01:07:45 +0700

    declare model & migration master_posisi

---
+++ migrations/20241002180711-create-master-posisi.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('master_posisis', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama_posisi: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('master_posisis');
+  }
+};
