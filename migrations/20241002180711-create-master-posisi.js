08767ca9bd47bd7ec1d10831c882e4b1cbfc4ac8
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-03 01:08:34 +0700

    add new coloumn

--- migrations/20241002180711-create-master-posisi.js
+++ migrations/20241002180711-create-master-posisi.js
@@ -12,6 +12,15 @@ module.exports = {
       nama_posisi: {
         type: Sequelize.STRING
       },
+      status: {
+        type: Sequelize.ENUM('Non Aktif', 'Aktif'),
+        defaultValue: 'Aktif',
+        allowNull: false,
+      },
+      created_id: {
+        type: Sequelize.TINYINT,
+        allowNull: true,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
