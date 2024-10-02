fa0bce254f9b56207cc7cf510ce677f24beb0d32
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-03 00:52:59 +0700

    change to enum and format it

--- migrations/20240930170430-create-karyawan.js
+++ migrations/20240930170430-create-karyawan.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable('karyawans', {
       id: {
         allowNull: false,
@@ -30,240 +30,263 @@ module.exports = {
       },
       agama: {
         allowNull: true,
-        type: Sequelize.TINYINT,
-        comments: 'islam'
+        type: Sequelize.ENUM(
+          'Islam',
+          'Kristen',
+          'Katolik',
+          'Hindu',
+          'Buddha',
+          'Konghucu'
+        )
       },
       jenis_kelamin: {
-        type: Sequelize.TINYINT,
+        type: Sequelize.ENUM('Pria', 'Wanita'),
         comment: '1 = Laki-laki, 2 = Perempuan',
-        allowNull: true,
+        allowNull: true
       },
       gol_darah: {
-        type: Sequelize.TINYINT,
-        comment: '1 = A, 2 = B, 3 = O, 4 = AB',
-        allowNull: true,
+        type: Sequelize.ENUM('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'),
+        allowNull: true
       },
       riwayat_penyakit: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_kk: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       kode_pos: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       nik_penduduk: {
         type: Sequelize.STRING,
         unique: true,
-        allowNull: true,
+        allowNull: true
       },
       alamat_ktp: {
         type: Sequelize.TEXT,
-        allowNull: true,
+        allowNull: true
       },
       alamat_domisili: {
         type: Sequelize.TEXT,
-        allowNull: true,
+        allowNull: true
       },
       pendidikan: {
-        type: Sequelize.TINYINT,
-        comment: '1 = SD, 2 = SMP, 3 = SMA, 4 = D1, 5 = D2, 6 = D3, 7 = D4, 8 = S1, 9 = S2, 10 = S3',
-        allowNull: true,
+        type: Sequelize.ENUM(
+          'SD',
+          'SMP',
+          'SMA',
+          'D1',
+          'D2',
+          'D3',
+          'D4',
+          'S1',
+          'S2',
+          'S3'
+        ),
+        allowNull: true
       },
       nama_sekolah: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       jurusan: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       email_pribadi: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_telp: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_wa: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_keluarga: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       hubungan_keluarga: {
-        type: Sequelize.TINYINT,
-        comment: '1 = Suami/Istri 2 = Ayah, 3 = Ibu, 4 = Kakak/Adik, 5 = Paman/Bibi, 6 = Kakek/Nenek',
-        allowNull: true,
+        type: Sequelize.ENUM(
+          'Suami/Istri',
+          'Ayah',
+          'Ibu',
+          'Kakak/Adik',
+          'Paman/Bibi',
+          'Kakek/Nenek'
+        ),
+        allowNull: true
       },
       status_pernikahan: {
-        type: Sequelize.TINYINT,
-        comment: '1 = Belum Menikah, 2 = Sudah Menikah, 3 = Janda, 4 = Duda',
-        allowNull: true,
+        type: Sequelize.ENUM('Belum Menikah', 'Sudah Menikah', 'Janda', 'Duda'),
+        allowNull: true
       },
       status_keluarga: {
-        type: Sequelize.TINYINT,
-        comment: '1 = Kepala Keluarga, 2 = Ibu, 3 = Anak ke 1, 4 = Anak ke 2, 5 = Anak ke 3, 6 = Lainya',
-        allowNull: true,
+        type: Sequelize.ENUM(
+          'Kepala Keluarga',
+          'Ibu',
+          'Anak ke 1',
+          'Anak ke 2',
+          'Anak ke 3',
+          'Lainnya'
+        ),
+        allowNull: true
       },
       jenis_sosmed: {
-        type: Sequelize.TINYINT,
-        comment: '1 = IG, 2 = FB, 3 = TikTok, 4 = YouTube, 5 = Twitter',
-        allowNull: true,
+        type: Sequelize.ENUM('IG', 'FB', 'TikTok', 'YouTube', 'Twitter'),
+        allowNull: true
       },
       nama_sosmed: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       // Data di perusahaan
       nik_karyawan: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       divisi_id: {
         type: Sequelize.BIGINT,
-        allowNull: true,
+        allowNull: true
       },
       pt_id: {
         type: Sequelize.BIGINT,
-        allowNull: true,
+        allowNull: true
       },
       jabatan_id: {
         type: Sequelize.BIGINT,
-        allowNull: true,
+        allowNull: true
       },
       posisi_id: {
         type: Sequelize.BIGINT,
-        allowNull: true,
+        allowNull: true
       },
       grade: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       tanggal_masuk: {
         type: Sequelize.DATE,
-        allowNull: true,
+        allowNull: true
       },
       status_kerja: {
-        type: Sequelize.TINYINT,
-        comment: '1 = Kontrak, 2 = Tetap, 3 = Training',
-        allowNull: true,
+        type: Sequelize.ENUM('Kontrak', 'Tetap', 'Training'),
+        allowNull: true
       },
       komposisi_peran: {
-        type: Sequelize.TINYINT,
-        comment: '1 = Support, 2 = Core',
-        allowNull: true,
+        type: Sequelize.ENUM('Support', 'Core'),
+        allowNull: true
       },
       komposisi_generasi: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       tanggal_kontrak: {
         type: Sequelize.DATE,
-        allowNull: true,
+        allowNull: true
       },
       tanggal_karyawan_tetap: {
         type: Sequelize.DATE,
-        allowNull: true,
+        allowNull: true
       },
       akhir_kontrak: {
         type: Sequelize.DATE,
-        allowNull: true,
+        allowNull: true
       },
       masa_kontrak: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       masa_kerja_bulan: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       masa_kerja_tahun: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       kota_rekruitmen: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       kota_penugasan: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       posisi_awal_diterima: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       email_internal: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_npwp: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_bpjs_kesehatan: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       no_bpjs_ketenagakerjaan: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       nama_bank: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       rekening: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       ukuran_baju: {
-        type: Sequelize.TINYINT,
-        comment: '1 = S, 2 = M, 3 = L, 4 = XL, 5 = XXL, 6 = Jumbo',
-        allowNull: true,
+        type: Sequelize.ENUM('S', 'M', 'L', 'XL', 'XXL', 'XXXL'),
+        allowNull: true
       },
       pengalaman_kerja_terakhir: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       jabatan_kerja_terakhir: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       foto: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       status_karyawan: {
-        type: Sequelize.TINYINT,
-        comment: '0 = Aktif, 1 = Resign, 2 = PHK',
-        defaultValue: 0,
-        allowNull: true,
+        type: Sequelize.ENUM('Aktif', 'Resign', 'PHK'),
+        defaultValue: 'Aktif',
+        allowNull: true
       },
       created_id: {
         type: Sequelize.TINYINT,
-        allowNull: true,
+        allowNull: true
       },
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        defaultValue: Sequelize.literal(
+          'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+        )
       }
-    });
+    })
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('karyawans');
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('karyawans')
   }
-};
+}
