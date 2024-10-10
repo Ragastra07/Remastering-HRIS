64e5b214b159d9b66bb5c7b174dd26748b9c1ed5
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-10 22:35:07 +0700

    addjust for a new route register

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controller/Users');
-
+const authController = require('../controller/Auth');
+// Register
+router.post('/register',authController.register);
 // Route for Users
 router.get('/user', userController.list);
-router.post('/user', userController.create);
 router.get('/user/:id', userController.retrieve);
 router.put('/user/:id', userController.update);
 router.delete('/user/:id', userController.destroy);
