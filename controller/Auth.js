d98fc68b14864583cb002efabb7ebcdcff25f303
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-11 20:44:16 +0700

    add login funct

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,16 +1,17 @@
 const model = require("../models/index");
 const bcrypt = require("bcrypt");
 module.exports = {
+	// Register a new user to the system.
   async register(req, res) {
     try {
       const { name, username, email, password } = req.body;
 
-      // Validate input data (add your validation logic here)
+      // * Validate input data (add your validation logic here)
 
-      // Hash the password
+      // * Hash the password
       const hashedPassword = await bcrypt.hash(password, 10); // Adjust the salt rounds as needed
 
-      // Create the user
+      // * Create the user
       const user = await model.User.create({
         name,
         username,
@@ -26,4 +27,33 @@ module.exports = {
       res.status(500).json({ error: error.message });
     }
   },
+	// Login an existing user to the system.
+	async login(req, res) {
+    try {
+      const { username, password } = req.body;
+
+      // * Find the user by username
+      const user = await model.User.findOne({ where: { username } });
+
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+
+      // * Compare the entered password with the hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+
+      // // Generate a JWT token
+      // const token = jwt.sign({ userId: user.id }, 'your_secret_key'); //not needed yet
+
+
+      res.status(200).json({ message: 'Login successful', user});
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
 };
