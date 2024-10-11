75586d694be77385dfb6d2e4316ab96356d2b452
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-12 01:57:16 +0700

    addjust for middleware

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controller/Users');
 const authController = require('../controller/Auth');
+const authenticateToken = require('../middleware/Authenticate');
+
+
 // Register
 router.post('/register',authController.register);
 // Login
 router.post('/login', authController.login);
 // Route for Users
-router.get('/user', userController.list);
+router.get('/user', authenticateToken, userController.list);
 router.get('/user/:id', userController.retrieve);
 router.put('/user/:id', userController.update);
 router.delete('/user/:id', userController.destroy);
