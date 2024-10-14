00e8936c2d91b1a78305294ea4fff1e15c753628
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-15 01:14:16 +0700

    create model

---
+++ models/Permission.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model, Sequelize
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Permission extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Permission.belongsToMany(models.Role, { through: 'permission-role' })
+    }
+  }
+  Permission.init({
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Permission',
+  });
+  return Permission;
+};
---
+++ models/Role.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model, Sequelize
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Role.belongsToMany(models.User, { through: 'role-user' });
+      Role.belongsToMany(models.Permission, { through: 'permission-role' });
+    }
+  }
+  Role.init({
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Role',
+  });
+  return Role;
+};
