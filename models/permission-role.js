b7ebefe8c0ba1c3c50cde75a8fcc910615e51d4c
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-15 01:16:58 +0700

    adjust useless

--- models/permission-role.js
+++ models/permission-role.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class permission - role extends Model {
+  class permissionrole extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
--- models/role-user.js
+++ models/role-user.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class role - user extends Model {
+  class roleuser extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
