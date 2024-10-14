6c87e9ab969fa90b86f7c72619016bdd415a51b8
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-15 00:30:53 +0700

    create relation tabel migration role-user

---
+++ migrations/20241014171540-create-role-user.js
@@ -0,0 +1,27 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('role-users', {
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      roleId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'roles',
+          key: 'id'
+        }
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('role-users');
+  }
+};
