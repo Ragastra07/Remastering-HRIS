759fd5eb76813a2b15d7a54da7ebc47f7839c52b
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-17 01:38:46 +0700

    create permission middleware

---
+++ middleware/CheckPermission.js
@@ -0,0 +1,28 @@
+const model = require('../models')
+
+module.exports = function (permissions) {
+  return async function (req, res, next) {
+    const user = await model.User.findByPk(req.user.id, {
+      include: {
+        model: model.Role,
+        include: model.Permission
+      }
+    })
+
+    if (!user) {
+      return res.status(403).json({ message: 'User not found' })
+    }
+
+    const hasPermission = user.Roles.some(role =>
+      role.Permissions.some(permission => permissions.includes(permission.name))
+    )
+
+    if (!hasPermission) {
+      return res
+        .status(403)
+        .json({ message: 'Access denied: insufficient permission' })
+    }
+
+    next()
+  }
+}
