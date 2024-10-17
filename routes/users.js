71c4c38671c58714411133d4334658501c7864db
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-18 03:32:18 +0700

    add Role Permission

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,22 @@ const authenticateToken = require('../middleware/Authenticate');
 router.post('/register',authController.register);
 // Login
 router.post('/login', authController.login);
+
+// Role
+router.get('/role', authenticateToken, roleController.index);
+router.post('/role', authenticateToken, roleController.store);
+router.put('/role/:id', authenticateToken, roleController.update);
+router.delete('/role/:id', authenticateToken, roleController.destroy);
+
+// Permission
+router.get('/permission', authenticateToken, permissionController.index);
+router.post('/permission', authenticateToken, permissionController.store);
+router.put('/permission/:id', authenticateToken, permissionController.update);
+router.delete('/permission/:id', authenticateToken, permissionController.destroy);
+
+// Route for Roles
+
+
 // Route for Users
 router.get('/user', authenticateToken, userController.list);
 router.get('/user/:id', userController.retrieve);
