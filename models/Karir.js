10748e016cb0b5adf48ee70e654399e416f8b2ac
Ragastra07 <haryo.accn.7@gmail.com>
2024-10-26 04:03:09 +0700

    rename

--- models/Karir.js
+++ models/Karir.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class karir extends Model {
+  class Karir extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,11 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  karir.init({
+  Karir.init({
     tgl_masuk: DataTypes.DATE
   }, {
     sequelize,
-    modelName: 'karir',
+    modelName: 'Karir',
   });
-  return karir;
+  return Karir;
 };
